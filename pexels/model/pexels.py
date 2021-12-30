5835e7a766a77b15eda8807565213958b26c7311
jazzrelax <jazzrelaxnow@gmail.com>
2021-12-30 12:08:33 -0300

    pexels model created

---
+++ pexels/model/pexels.py
@@ -0,0 +1,27 @@
+class Pexels:
+    """This class is the model about a Pexels Image
+    download.
+
+    Author:
+        jazzrelax
+    """
+
+    def __init__(self):
+        """New Pexels Download.
+        """
+        self._urlimages = list()
+        self._pathdir = str()
+
+    # getters and setters
+
+    @property
+    def urlimages(self) -> list:
+        return self._urlimages
+
+    @property
+    def pathdir(self):
+        return self._pathdir
+
+    @pathdir.setter
+    def pathdir(self, pathdir: str):
+        self._pathdir = pathdir
