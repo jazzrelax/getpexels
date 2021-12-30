0b35e8a574a8ad5a0af5b2edbac4c378a8564a76
jazzrelax <jazzrelaxnow@gmail.com>
2021-12-30 12:56:10 -0300

    getpexels structure

--- pexels/download/getpexels.py
+++ pexels/download/getpexels.py
@@ -1,3 +1,4 @@
+from requests import codes, get as getpexel
 from pexels.model.pexels import Pexels
 
 
@@ -21,6 +22,11 @@ class GetPexels:
 
     @property
     def pexelshtml(self):
+        """Path to save pexels webpage.
+
+        Returns:
+            str: place to save pexelshtml.
+        """
         return self._pexelshtml
 
     # main methods
@@ -28,6 +34,18 @@ class GetPexels:
     def download_pexels(self, pexels: Pexels) -> bool:
         """This method checks and try to make download
         from Pexels images.
+
+        Args:
+            pexels (Pexels): Model to download.
+
+        Returns:
+            bool: True if downloaded else False.
+        """
+        pass
+
+    def pexels_html(self, pexels: Pexels) -> bool:
+        """This method checks and try to make download
+        from Pexels webpage html.
 
         Args:
             pexels (Pexels): Model to download.
@@ -48,3 +66,52 @@ class GetPexels:
             tuple: with images str url.
         """
         pass
+
+    # private methods # download html | download images
+
+    def _gethtmlpexels(self, htmlurl: str, filepath: str) -> bool:
+        """This method is to make download froma pexels webpage.
+
+        Args:
+            htmlurl (str): url from pexels page.
+            filepath (str): path to file and file name.
+
+        Returns:
+            bool: True if success else False.
+        """
+        # getting the pexels page html
+        response = getpexel(url=htmlurl, stream=True)
+        response.encoding = 'utf-8'
+        # check if it exists
+        if response.status_code == codes.OK:
+            # it exist
+            with open(file=filepath, mode='w') as pexels:
+                pexels.writelines(
+                    response.iter_content(decode_unicode='utf-8'))
+            return True
+        else:
+            # it doesn't exist
+            return False
+
+    def _getimagepexels(self, imageurl: str, filepath: str) -> bool:
+        """This method is to make download froma pexels images.
+
+        Args:
+            imageurl (str): url from pexels image.
+            filepath (str): path to file and file name.
+
+        Returns:
+            bool: True if success else False.
+        """
+        # getting the pexels page html
+        response = getpexel(url=htmlurl, stream=True)
+        # check if it exists
+        if response.status_code == codes.OK:
+            # it exist
+            with open(file=filepath, mode='wb') as pexels:
+                for byte in response.iter_content(chunk_size=256):
+                    pexels.write(byte)
+            return True
+        else:
+            # it doesn't exist
+            return False
